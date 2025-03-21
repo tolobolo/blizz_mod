9ef6f91f8c128d02fe0c4596e4a81a26b00adb51
Emrys <emrys@local>
2025-03-21 19:27:07 +0100

    add timer for speed_gem

--- src/main/java/my/hector_tolobolo/gems_mod/Speed_gem.java
+++ src/main/java/my/hector_tolobolo/gems_mod/Speed_gem.java
@@ -5,22 +5,26 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
-import net.minecraft.text.Text;
 
 public class Speed_gem extends Base_gem {
+    public static boolean use_attack = false;
+
     public Speed_gem(Settings settings) {
         super(settings);
     }
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (hand == Hand.MAIN_HAND && !world.isClient)  {
+        if (hand == Hand.MAIN_HAND && !world.isClient && !use_attack) {
             user.sendMessage(Text.of("HASTE"));
             user.addStatusEffect(new StatusEffectInstance(
                     StatusEffects.HASTE, 200, 3));
+            use_attack = true;
+            Base_gem.CountDown("100", use_attack);
         }
 
 
@@ -32,9 +36,7 @@ public class Speed_gem extends Base_gem {
         if (entity instanceof PlayerEntity player && !world.isClient) {
             if (player.getOffHandStack() == stack) {
                 player.addStatusEffect(new StatusEffectInstance(
-                        StatusEffects.SPEED, 60, 0, true, false));
-                player.addStatusEffect(new StatusEffectInstance(
-                        StatusEffects.HASTE, 60, 0, true, false));
+                        StatusEffects.SPEED, 60, 1, true, false));
             }
         }
 
